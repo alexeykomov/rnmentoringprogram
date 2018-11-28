04a964d06ff6e3de24964b2c2fbc4f071d6e79c4
alexeykomov <alexeykcontact@gmail.com>
2018-11-28 20:32:27 +0300

    add: scenes, color constants

---
+++ src/colors.js
@@ -0,0 +1,23 @@
+/**
+ * Colors.
+ *
+ * @format
+ * @flow
+ */
+
+const Colors = {
+  EpamBlue: '#76cdd8',
+  LimeGreen: '#cedb56',
+  Graphite: '#222222',
+  White: '#ffffff',
+  Coral: '#d35d47',
+  BrightBlue: '#008ace',
+  SharpBlue: '#39c2d7',
+  DarkBlue: '#263852',
+  LightGray: '#cccccc',
+  DarkGray: '#464557',
+};
+
+export type Color = $Keys<typeof Colors>;
+
+export default Colors;
