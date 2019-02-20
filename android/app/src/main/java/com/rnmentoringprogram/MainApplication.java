adc24b0eca751a1879e48d3256fd35a7efd3f990
alexeykomov <alexeykofficial@gmail.com>
2019-02-20 11:02:00 +0300

    [WIP] add: lining android module

--- android/app/src/main/java/com/rnmentoringprogram/MainApplication.java
+++ android/app/src/main/java/com/rnmentoringprogram/MainApplication.java
@@ -3,6 +3,7 @@ package com.rnmentoringprogram;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import me.alexeykomov.RNRnmentoringprogramAsyncStorage.RNRnmentoringprogramAsyncStoragePackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.airbnb.android.react.lottie.LottiePackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNRnmentoringprogramAsyncStoragePackage(),
             new RNDeviceInfo(),
             new LottiePackage(),
             new SplashScreenReactPackage(),
