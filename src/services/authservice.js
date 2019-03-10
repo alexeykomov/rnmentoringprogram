60da76394edcb4cfe7c3bacf6ea29bc207a237de
alexeykomov <alexeykofficial@gmail.com>
2019-03-10 21:10:42 +0300

    [WIP] add: services

---
+++ src/services/authservice.js
@@ -0,0 +1,72 @@
+// @flow
+import { Sentry } from 'react-native-sentry';
+import RNRnmentoringprogramAsyncStorage from 'react-native-rnmentoringprogram-async-storage';
+
+const AUTH_URL =
+  'http://ecsc00a02fb3.epam.com/index.php/rest/V1/integration/customer/token';
+
+export const sendAuthRequest = async (
+  username: string,
+  password: string,
+  retryAction: Function,
+  handleRequestSuccess: () => void,
+  handleRequestError: (Error, Function) => void,
+) => {
+  try {
+    // const response = await this.mockResponse();
+    const response = await getToken(username, password);
+    const responseIsOk = response.ok;
+    if (!responseIsOk) {
+      return handleRequestError(new Error('Response is not ok.'), retryAction);
+    }
+    const token = await response.text();
+    console.log('token: ', token);
+    await saveLoginInfo(token, username, password);
+    handleRequestSuccess();
+  } catch (e) {
+    Sentry.captureException(e);
+    handleRequestError(e, retryAction);
+  }
+};
+
+const saveLoginInfo = async (
+  token: string,
+  username: string,
+  password: string,
+) => {
+  try {
+    await Promise.all([
+      RNRnmentoringprogramAsyncStorage.setItem('token', token),
+      RNRnmentoringprogramAsyncStorage.setItem('username', username),
+      RNRnmentoringprogramAsyncStorage.setItem('password', password),
+    ]);
+  } catch (error) {
+    Sentry.captureException(error);
+    console.log("Error - token wasn't saved: ", error);
+  }
+};
+
+export const mockGetToken = async (): GetTokenResponseType => {
+  return Promise.resolve({ ok: true, text: () => Promise.resolve('token') });
+};
+
+export const getToken = (
+  username: string,
+  password: string,
+): GetTokenResponseType => {
+  return fetch(AUTH_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
+};
+
+type GetTokenResponseType = Promise<{|
+  ok: boolean,
+  text: () => Promise<string>,
+|}>;
